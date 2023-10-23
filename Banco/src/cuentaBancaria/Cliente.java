01c91ee820ffc334521ef61015a96e85cd2022d6
angelogarcia74 <142682904+angelogarcia74@users.noreply.github.com>
2023-10-22 20:15:14 -0500

    CuentaBancaria

--- Banco/src/cuentaBancaria/Cliente.java
+++ Banco/src/cuentaBancaria/Cliente.java
@@ -1,107 +1,77 @@
-package com.example.tfts3.Models;
+package CuentaBancaria;
 
-import java.io.Serializable;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.multipart.MultipartFile;
+public class CuentaBancaria {
+    private String numeroCuenta;
+    private String descripcionCuenta;
+    private String cuentaCCI;
+    private double saldoDisponible;
+    private List<MovimientoCuenta> movimientos;
+    private String estado;
 
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-
-@Entity
-@Table(name="clientes")
-public class Cliente implements Serializable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@NotEmpty
-	private String nombre;
-	@NotEmpty
-	private String apellidos;
-	@NotEmpty
-	@Email
-	private String email;
-	@NotNull
-	@Column(name="create_at")
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date createAt;
-	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Facturas> facturas;
-	@NotEmpty
-	private String foto;
-	@Transient
-	private MultipartFile file;
-	private static final long serialVersionUID=1L;
-
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellidos() {
-		return apellidos;
-	}
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Date getCreateAt() {
-		return createAt;
-	}
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
-	public List<Facturas> getFacturas() {
-		return facturas;
-	}
-	public void setFacturas(List<Facturas> facturas) {
-		this.facturas = facturas;
-	}
-	public String getFoto() {
-		return foto;
-	}
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	@Override
-	public String toString(){
-		return nombre+" "+apellidos;
-	}
-    public MultipartFile getFile() {
-        return file;
+    public CuentaBancaria(String numeroCuenta, String descripcionCuenta, String cuentaCCI, double saldoDisponible, String estado) {
+        this.numeroCuenta = numeroCuenta;
+        this.descripcionCuenta = descripcionCuenta;
+        this.cuentaCCI = cuentaCCI;
+        this.saldoDisponible = saldoDisponible;
+        this.movimientos = new ArrayList<>();
+        this.estado = estado;
     }
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }	
-}
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getDescripcionCuenta() {
+        return descripcionCuenta;
+    }
+
+    public void setDescripcionCuenta(String descripcionCuenta) {
+        this.descripcionCuenta = descripcionCuenta;
+    }
+
+    public String getCuentaCCI() {
+        return cuentaCCI;
+    }
+
+    public void setCuentaCCI(String cuentaCCI) {
+        this.cuentaCCI = cuentaCCI;
+    }
+
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(double saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+
+    public List<MovimientoCuenta> getMovimientos() {
+        return movimientos;
+    }
+
+    public void setMovimientos(List<MovimientoCuenta> movimientos) {
+        this.movimientos = movimientos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria [Número de Cuenta: " + numeroCuenta + 
+               ", Descripción: " + descripcionCuenta + 
+               ", CCI: " + cuentaCCI + 
+               ", Saldo Disponible: " + saldoDisponible + 
+               ", Estado: " + estado + "]";
