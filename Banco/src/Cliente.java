700805b8e1098b74a05154e89cf8fc696a175cb6
angelogarcia74 <142682904+angelogarcia74@users.noreply.github.com>
2023-10-22 19:42:46 -0500

    Update Cliente.java

--- Banco/src/Cliente.java
+++ Banco/src/Cliente.java
@@ -1,107 +1,133 @@
-package com.example.tfts3.Models;
-
-import java.io.Serializable;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.multipart.MultipartFile;
+public class Cliente {
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String dni;
+    private List<CuentaBancaria> cuentas;
+    private List<EventoBancario> eventoBancarios;
 
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-
-@Entity
-@Table(name="clientes")
-public class Cliente implements Serializable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@NotEmpty
-	private String nombre;
-	@NotEmpty
-	private String apellidos;
-	@NotEmpty
-	@Email
-	private String email;
-	@NotNull
-	@Column(name="create_at")
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date createAt;
-	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Facturas> facturas;
-	@NotEmpty
-	private String foto;
-	@Transient
-	private MultipartFile file;
-	private static final long serialVersionUID=1L;
-
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellidos() {
-		return apellidos;
-	}
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Date getCreateAt() {
-		return createAt;
-	}
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
-	public List<Facturas> getFacturas() {
-		return facturas;
-	}
-	public void setFacturas(List<Facturas> facturas) {
-		this.facturas = facturas;
-	}
-	public String getFoto() {
-		return foto;
-	}
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	@Override
-	public String toString(){
-		return nombre+" "+apellidos;
-	}
-    public MultipartFile getFile() {
-        return file;
+    public Cliente(int idCliente, String nombre, String apellido, String direccion, String telefono, String dni) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.dni = dni;
+        this.cuentas = new ArrayList<>();
+        this.eventoBancarios = new ArrayList<>();
     }
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }	
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public List<CuentaBancaria> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(List<CuentaBancaria> cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public List<EventoBancario> getEventoBancarios() {
+        return eventoBancarios;
+    }
+
+    public void setEventoBancarios(List<EventoBancario> eventoBancarios) {
+        this.eventoBancarios = eventoBancarios;
+    }
+
+    public void agregarCuenta(CuentaBancaria cuenta) {
+        cuentas.add(cuenta);
+    }
+
+    public void agregarEventoBancario(EventoBancario evento) {
+        eventoBancarios.add(evento);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [IdCliente: " + idCliente + ", Nombre: " + nombre + ", Apellido: " + apellido +
+               ", Dirección: " + direccion + ", Teléfono: " + telefono + ", DNI: " + dni + "]";
+    }
+}
+
+public class CuentaBancaria {
+    private int numeroCuenta;
+    private double saldo;
+
+    public CuentaBancaria(int numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria [Número de Cuenta: " + numeroCuenta + ", Saldo: " + saldo + "]";
+    }
 }
